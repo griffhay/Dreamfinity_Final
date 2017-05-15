fa82ff4531af85ed413858dd5a8715ce70b723d6
MarcusDixon <mdixon@csumb.edu>
2017-05-14 17:16:29 -0700

    General Unity Files

---
+++ Dreamfinity/Assets/_Assets/_Scripts/AimLucidity.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Invector.CharacterController;
+
+public class AimLucidity : MonoBehaviour {
+
+    v3rdPersonCamera camControlRef;
+    GameObject camObjRef;
+    Camera camRef;
+    CharacterControler charContRef;
+    bool preAim;
+    GameObject paintBrush;
+    Transform paintBrishAimTransRef;
+
+
+    public void Awake()
+    {
+        camControlRef = GameObject.FindWithTag("MainCamera").GetComponent<v3rdPersonCamera>();
+        charContRef = GameObject.FindWithTag("Player").GetComponent<CharacterControler>();
+        camObjRef = (GameObject.FindWithTag("MainCamera"));
+        camRef = camObjRef.GetComponent<Camera>();
+    }
+
+    public void Start()
+    {
+        preAim = true;
+    }
+    void Update()
+    {
+        Aim(Input.GetAxisRaw("Aim"));
+       
+    }
+
+    public void Aim(float aimAxisValue)
+    {
+        if(aimAxisValue != 0)
+        {
+            preAim = false;
+            
+            charContRef.ToggleCharacterlook(true);
+            camControlRef.defaultDistance = 1f;
+            camControlRef.rightOffset = 0.7f;
+            camControlRef.height = .6f;
+
+        }
+        else
+        {
+            if(!preAim)
+            {
+                charContRef.ToggleCharacterlook(false);
+                camControlRef.defaultDistance = 7;
+                camControlRef.rightOffset = 0.25f;
+                camControlRef.height = 1f;
+            }
+            
+        }
+        
+    }
+        
+}
---
+++ Dreamfinity/Assets/_Assets/_Scripts/RespawnManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnManager : MonoBehaviour {
+
+    public int spawnLocation;
+    public Vector3[] SpawnPoints;
+    Rigidbody rigidBodyRef;
+    PlayerHP playerHPRef;
+    LucidityControl lucidityControlRef;
+    SheepCounterManager sheepCountRef;
+    SpawnPoint spawnPointRef;
+    public int CurrentHP;
+    public int MaxHP;
+    public int CurrentLucidity;
+    public int MaxLucidity;
+    public int SheepCount;
+    public int SpawnLoc;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        rigidBodyRef = GetComponent<Rigidbody>();
+        playerHPRef = GetComponent<PlayerHP>();
+        lucidityControlRef = GetComponent<LucidityControl>();
+        sheepCountRef = GameObject.FindWithTag("SheepCounterText").GetComponent<SheepCounterManager>();
+        spawnPointRef = GameObject.FindWithTag("SpawnPoint").GetComponent<SpawnPoint>();
+
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        SaveSpawn();
+        SpawnPlayer();
+
+    }
+
+    public void SpawnPlayer()
+    {
+        rigidBodyRef.position = SpawnPoints[SpawnLoc];
+        Debug.Log(SpawnPoints[SpawnLoc]);
+        playerHPRef.maxHealth = MaxHP;
+        playerHPRef.currentHealth = CurrentHP;
+        lucidityControlRef.limit = MaxLucidity;
+        lucidityControlRef.balance = CurrentLucidity;
+        sheepCountRef.sheepCount = SheepCount;
+
+    }
+
+    public void SaveSpawn()
+    {
+        CurrentHP = playerHPRef.currentHealth;
+        MaxHP = playerHPRef.maxHealth;
+        CurrentLucidity = lucidityControlRef.balance;
+        MaxLucidity = lucidityControlRef.limit;
+        SheepCount = sheepCountRef.sheepCount;
+        SpawnLoc = spawnPointRef.spawnPoint;
+        Debug.Log("Help Me");
+
+    }
+
+    /* 
+     private void SpawnPlayer(Vector3 newPos)
+   {
+       rigidBodyRef.position = newPos;
+   }
+   */
+
+}
